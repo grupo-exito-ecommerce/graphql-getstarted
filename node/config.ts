22786ae2d4440bdde96f64b2e0956b0f91f7ba61
Maik Restrepo <stiven3345@hotmail.com>
2019-03-20 13:55:51 -0500

    Remove config file.

--- node/config.ts
+++
@@ -1,8 +0,0 @@
-export const config: Config = {
-  SmartQuickUrl:
-    'http://www.smartquick.com.co/restexito/servicio_rest/MantieneReceptor/consulta_direccion_zona/'
-};
-
-interface Config {
-  SmartQuickUrl: String;
-}
